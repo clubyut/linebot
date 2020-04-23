707161acc1797b7c303f199d35a6940f3fe3aa00
clubyut <36685402+clubyut@users.noreply.github.com>
2020-04-23 16:27:35 +0700

    Create index - Copy (2).php

---
+++ index - Copy (2).php
@@ -0,0 +1,100 @@
+<?php
+//mysql://b79cc14ad249eb:76b0ba67@us-cdbr-iron-east-01.cleardb.net/heroku_9899d38b5c56894?reconnect=true
+$access_token = 'yK9Mley/uEEGeEeVjkR2UHggFuwqO1yeg149LN0lUSG5/NgXxcgwYgzm3A5FOp+SfPbpCESrotui1CLv2YEdcsirvcKET+u8EaPNPHhVWdIGJgUewZYFbq6lOZzhftK6akBtUm2rkFOyUVdL1B/URwdB04t89/1O/w1cDnyilFU=';
+  $LINEData = file_get_contents('php://input');
+  $LINEDatas['token'] = $access_token;
+  $jsonData = json_decode($LINEData,true);
+
+  $replyToken = $jsonData["events"][0]["replyToken"];
+  $userID = $jsonData["events"][0]["source"]["userId"];
+  $text = $jsonData["events"][0]["message"]["text"];
+  $timestamp = $jsonData["events"][0]["timestamp"]; //$results['response']
+  $mID = $jsonData["events"][0]["message"]["id"];
+  $mType =$jsonData["events"][0]["message"]["type"];
+  $servername = "us-cdbr-iron-east-01.cleardb.net";
+  $username = "b79cc14ad249eb";
+  $password = "76b0ba67";
+  $dbname = "heroku_9899d38b5c56894";
+  $mysql = new mysqli($servername, $username, $password, $dbname);
+  mysqli_set_charset($mysql, "utf8");
+
+  if ($mysql->connect_error){
+  $errorcode = $mysql->connect_error;
+  print("MySQL(Connection)> ".$errorcode);
+  }
+
+  function sendMessage($replyJson, $sendInfo){
+          $ch = curl_init($sendInfo["URL"]);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              'Content-Type: application/json',
+              'Authorization: Bearer ' . $sendInfo["AccessToken"])
+              );
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+          $result = curl_exec($ch);
+          curl_close($ch);
+    return $result;
+  }
+  function getContent($datas)
+{
+	$datasReturn = [];
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.line.me/v2/bot/message/" . $datas['messageId'] . "/content",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_POSTFIELDS => "",
+		CURLOPT_HTTPHEADER => array(
+			"Authorization: Bearer " . $datas['token'],
+			"cache-control: no-cache"
+		),
+	));
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+	curl_close($curl);
+
+	if ($err) {
+		$datasReturn['result'] = 'E';
+		$datasReturn['message'] = $err;
+	} else {
+		$datasReturn['result'] = 'S';
+		$datasReturn['message'] = 'Success';
+		$datasReturn['response'] = $response;
+	}
+
+	return $datasReturn;
+}
+
+  $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`,`image`) VALUES ('$userID','$text','$timestamp','$image')");
+
+  $getUser = $mysql->query("SELECT * FROM `Customer` WHERE `UserID`='$userID'");
+  $getuserNum = $getUser->num_rows;
+  $replyText["type"] = "text";
+  if ($getuserNum == "0"){
+    $replyText["text"] = "สวัสดีคับบบบ";
+  } else {
+    while($row = $getUser->fetch_assoc()){
+      $Name = $row['Name'];
+      $Surname = $row['Surname'];
+      $CustomerID = $row['CustomerID'];
+    }
+    $replyText["text"] = "สวัสดีคุณ $Name $Surname (#$CustomerID)";
+  }
+
+  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+  $lineData['AccessToken'] = "yK9Mley/uEEGeEeVjkR2UHggFuwqO1yeg149LN0lUSG5/NgXxcgwYgzm3A5FOp+SfPbpCESrotui1CLv2YEdcsirvcKET+u8EaPNPHhVWdIGJgUewZYFbq6lOZzhftK6akBtUm2rkFOyUVdL1B/URwdB04t89/1O/w1cDnyilFU=";
+
+  $replyJson["replyToken"] = $replyToken;
+  $replyJson["messages"][0] = $replyText;
+
+  $encodeJson = json_encode($replyJson);
+
+  $results = sendMessage($encodeJson,$lineData);
+  echo $results;
+  http_response_code(200);
