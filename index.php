7aa661d4e118004e056eb786980e72ae2a2d9b1d
clubyut <36685402+clubyut@users.noreply.github.com>
2020-05-05 10:13:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -155,6 +155,40 @@ function pushMsg($arrayHeader,$arrayPostData){
       $result = curl_exec($ch);
       curl_close ($ch);
    }
+    function getLINEProfile($datas)
+{
+   $datasReturn = [];
+   $curl = curl_init();
+   curl_setopt_array($curl, array(
+     CURLOPT_URL => $datas['url'],
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_ENCODING => "",
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 30,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => "GET",
+     CURLOPT_HTTPHEADER => array(
+       "Authorization: Bearer ".$datas['token'],
+       "cache-control: no-cache"
+     ),
+   ));
+   $response = curl_exec($curl);
+   $err = curl_error($curl);
+   curl_close($curl);
+   if($err){
+      $datasReturn['result'] = 'E';
+      $datasReturn['message'] = $err;
+   }else{
+      if($response == "{}"){
+          $datasReturn['result'] = 'S';
+          $datasReturn['message'] = 'Success';
+      }else{
+          $datasReturn['result'] = 'E';
+          $datasReturn['message'] = $response;
+      }
+   }
+   return $datasReturn;
+}
  //ADD_Q
 $replyText["type"] = "text";
 if($text== 'ADD_Q')
@@ -605,37 +639,4 @@ $branchNo,
   http_response_code(200);
 
 
-  function getLINEProfile($datas)
-{
-   $datasReturn = [];
-   $curl = curl_init();
-   curl_setopt_array($curl, array(
-     CURLOPT_URL => $datas['url'],
-     CURLOPT_RETURNTRANSFER => true,
-     CURLOPT_ENCODING => "",
-     CURLOPT_MAXREDIRS => 10,
-     CURLOPT_TIMEOUT => 30,
-     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-     CURLOPT_CUSTOMREQUEST => "GET",
-     CURLOPT_HTTPHEADER => array(
-       "Authorization: Bearer ".$datas['token'],
-       "cache-control: no-cache"
-     ),
-   ));
-   $response = curl_exec($curl);
-   $err = curl_error($curl);
-   curl_close($curl);
-   if($err){
-      $datasReturn['result'] = 'E';
-      $datasReturn['message'] = $err;
-   }else{
-      if($response == "{}"){
-          $datasReturn['result'] = 'S';
-          $datasReturn['message'] = 'Success';
-      }else{
-          $datasReturn['result'] = 'E';
-          $datasReturn['message'] = $response;
-      }
-   }
-   return $datasReturn;
-}
+ 
