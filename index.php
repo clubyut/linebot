139d94571983d54300aa5026d0bf9f9ec3c45924
clubyut <36685402+clubyut@users.noreply.github.com>
2020-05-20 23:35:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -375,7 +375,7 @@ $mysql->query("INSERT INTO `user_action`(`u_id`,`action`)VALUES('$userID','$acti
       
                                   if(strlen($tel)<>10)
                                     {
-      	                                $replyText["text"] = "กรุณากรอกชื่อ เว้นวรรค ตามด้วยเบอร์โทรลูกค้าด้วยค่ะ";
+      	                                $replyText["text"] = "กรุณากรอกชื่อ เว้นวรรค ตามด้วยเบอร์โทรลูกค้าด้วยค่ะ $START_Q";
                                     }else{
                                     	//// ADD_Q ลุกค้า โดย Admin
 
